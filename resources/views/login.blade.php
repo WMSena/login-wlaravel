e186e642299f7eb1526b8dca90b52ab6f5404efd
Rifky <rifky.rangkuti@gmail.com>
2017-09-04 23:51:50 -0700

    FrontEnd

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,1 +1,32 @@
-hello
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+    <title>WAMPLO</title>
+</head>
+<body>
+<div class="wrapper">
+    <form action="test.php">
+        <div class="container">
+        <i class="fa fa-envelope-o"></i>
+                <input type="text" name="user" id="user" class="form-log" placeholder="Email">
+        </div>
+        <div class="container">
+            <i class="fa fa-lock"></i>
+                <input type="password" name="pass" id="pass" class="form-log" placeholder="Password">
+        </div>
+        <div class="container">
+                <input type="submit" value="LOGIN" class="btn-log pull-left">
+                <input type="reset" value="RESET" class="btn-log pull-right">
+        </div>
+    
+    </form>
+
+</div>
+    
+</body>
+</html>
