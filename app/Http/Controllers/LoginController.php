9547d690da708d8803b8414ea1064c1b18616fb3
nadiasarah <nadiasarahp@gmail.com>
2017-09-05 13:37:56 +0700

    controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    function getLogin()
+    {
+      return view('login');
+    }
+
+
+    function PostLogin()
+    {
+
+    }
+}
